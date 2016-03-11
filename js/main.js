fd93e18058afc3abb6b64e0fb4c7f3225e62d8d5
Kyle-Jackson <kylejackson@Kyles-MacBook-Pro.local>
2016-03-11 10:49:59 -0500

    completed.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
+var $header = $('header');
 var $h1 = $('h1');
-var $ul = $('ul');
+var $main = $('main');
+var $h2 = $('h2');
+var $images = $('img');
+var $dl = $('dl');
+var $dt = $('dt');
+var $li = $('li');
+var $mainlist = $('main ul');
 
-$h1.html('Sea Slugs');
-$ul.addClass('slug-list');
-
-
-slug.forEach(function (slug) {
-  var $li = $('<li>');
-  var $figure = $('<figure>');
-  var $img = $('<img>');
-  var $caption = $('<figcaption>');
-
-  $figure.append($img, $caption)
-  $li.append($figure)
-  $ul.append($li);
-
-});
+$header.addClass('masthead');
+$h1.removeClass('big-heading').addClass('logo');
+$main.addClass('wrapper');
+$('main p:first-child').addClass('intro');
+$h2.addClass('slug-head');
+$mainlist.addClass('slug-list');
+$images.addClass('slug-img');
+$('ul li:first-child').addClass('slug-list-first');
+$('uil li:last-child').addClass('slug-list-last');
+$dl.addClass('classification');
+$dt.removeClass('big-label');
+$('ul li:last-child').show();
